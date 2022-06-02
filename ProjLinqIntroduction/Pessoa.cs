ff63f416163b87074ac348e4bcb428d5d5bf2a6a
= <andre.silva@hubdoctor.com.br>
2022-06-02 20:49:58 -0300

    Initial Created

---
+++ ProjLinqIntroduction/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjLinqIntroduction
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public List<Pessoa> Pessoas()
+        {
+            List<Pessoa> lst = new List<Pessoa>();
+            //Gerar uma massa de dados
+            lst.Add(new Pessoa() { Id = 1, Name = "André Silva", Age = 35 });
+            lst.Add(new Pessoa() { Id = 2, Name = "Daniel Silva", Age = 1 });
+            lst.Add(new Pessoa() { Id = 3, Name = "Felipe Pestana", Age = 18 });
+            lst.Add(new Pessoa() { Id = 4, Name = "Fabio Papine", Age = 40 });
+            return lst;
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + this.Id + " - " + "Nome: " + this.Name  + " - " + "Idade: " + this.Age;
+        }
+
+    }
+}
---
+++ ProjLinqIntroduction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace ProjLinqIntroduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine("****Exemplo Lista Full****");
+
+            foreach (var p in pessoa.Pessoas())
+            {
+                 Console.WriteLine(p);
+            }
+
+            Console.WriteLine("****Exemplo Lista Linq****");
+
+            var pessoas =
+                from p in pessoa.Pessoas()
+                where p.Age < 18
+                select p;
+
+            foreach (var item in pessoas)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("****Exemplo Lista Lambda****");
+
+            pessoa.Pessoas().Where(p => p.Age < 18).ToList().ForEach(p => Console.WriteLine(p));
+
+        }
+    }
+}
